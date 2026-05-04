d406b53dc5a35ccf110663d95be8cb3a4ff4a503
Akinbori <87369242+Akinbori@users.noreply.github.com>
2026-05-04 21:51:52 +0100

    Update source_registry.py

--- app/adapters/source_registry.py
+++ app/adapters/source_registry.py
@@ -2,6 +2,7 @@ import os
 
 from .live_sources import GreenhouseBoardAdapter, LeverPostingsAdapter
 from .workable_sources import WorkablePublicJobsAdapter
+from .ashby_sources import AshbyJobsAdapter
 from .social_sources import RedditHiringSignalAdapter, HackerNewsHiringSignalAdapter
 from .search_sources import SerpApiHiringSignalAdapter
 
@@ -26,6 +27,11 @@ DEFAULT_WORKABLE_COMPANIES = [
     "pavago",
 ]
 
+DEFAULT_ASHBY_COMPANIES = [
+    "ashby",
+    "levels.fyi",
+]
+
 
 def _split_env(name: str) -> list[str]:
     raw = os.getenv(name, "")
@@ -36,6 +42,7 @@ def build_default_adapters():
     greenhouse_boards = _split_env("GREENHOUSE_BOARD_TOKENS") or DEFAULT_GREENHOUSE_BOARDS
     lever_companies = _split_env("LEVER_COMPANIES") or DEFAULT_LEVER_COMPANIES
     workable_companies = _split_env("WORKABLE_COMPANIES") or DEFAULT_WORKABLE_COMPANIES
+    ashby_companies = _split_env("ASHBY_COMPANIES") or DEFAULT_ASHBY_COMPANIES
 
     adapters = []
 
@@ -47,6 +54,9 @@ def build_default_adapters():
 
     for company in workable_companies:
         adapters.append(WorkablePublicJobsAdapter(subdomain=company))
+
+    for company in ashby_companies:
+        adapters.append(AshbyJobsAdapter(company=company))
 
     adapters.append(RedditHiringSignalAdapter())
     adapters.append(HackerNewsHiringSignalAdapter())
